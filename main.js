ae284bb0f86ef0abce9800ee0c1f041497416f53
unknown <wakame@obirin.ac.jp>
2020-07-09 20:30:14 +0900

    あなたは誰？チャットスタート

--- main.js
+++ main.js
@@ -3,9 +3,20 @@ const socketio = io();
     const input = document.getElementById("msg");
     const chats = document.getElementById("chats");
 
+   const nameform = document.getElementyById("nameform");
+   const name = document.getElementById("name");
+
+   let username='';
+   nameform.addEventListener('submit', function(event){
+       username = name.value;
+       event.preventDefault();
+       nameform.style.display ="none";
+       form.style.display ="block"
+   })
+
     form.addEventListener('submit', function(event){
-        const msg = JSON.stringify({msg: input.value, name: 'user'})
-      socketio.emit('message', mag);
+        const msg = JSON.stringify({msg: input.value, name: username})
+      socketio.emit('message', msg);
       input.value='';
       event.preventDefault();
     })
