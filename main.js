938dd50a5c6d175d76f9c7c13617b9a07fd3b49c
unknown <wakame@obirin.ac.jp>
2020-07-09 20:30:14 +0900

    修正

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 const socketio = io();
-    const form = document.getElementById("form");
-    const input = document.getElementById("msg");
-    const chats = document.getElementById("chats");
+const form = document.getElementById("form");
+const input = document.getElementById("msg");
+const chats = document.getElementById("chats");
 
-    form.addEventListener('submit', function(event){
-        const msg = JSON.stringify({msg: input.value, name: 'user'})
-      socketio.emit('message', mag);
-      input.value='';
-      event.preventDefault();
-    })
+const nameform = document.getElementById("nameform");
+const name = document.getElementById("name");
+
+let username='';
+nameform.addEventListener('submit', function(event){
+  username = name.value;
+  event.preventDefault();
+  nameform.style.display ="none";
+  form.style.display ="block";
+ })
+
+form.addEventListener('submit', function(event){
+  const msg = JSON.stringify({msg: input.value, name: username})
+  socketio.emit('message', msg);
+  input.value='';
+  event.preventDefault();
+ })
     socketio.on('message',function(msg){
         const obj = JSON.parse(msg);
 
@@ -16,6 +27,7 @@ const socketio = io();
       const dd = document.createElement("dd");
       dt.append(obj.name);
       chats.append(dt);
-      dd.append(obj.msg)
+      dd.append(obj.msg);
       chats.append(dd);
     });
+    
