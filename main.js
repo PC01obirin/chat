729335a422717500bce18f26fe84b9a6f63a9c25
unknown <wakame@obirin.ac.jp>
2020-07-10 00:31:25 +0900

    間違いないはず

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 const socketio = io();
-    const form = document.getElementById("form");
-    const input = document.getElementById("msg");
-    const chats = document.getElementById("chats");
+const form = document.getElementById("form");
+const input = document.getElementById("msg");
+const chats = document.getElementById("chats");
 
-   const nameform = document.getElementyById("nameform");
-   const name = document.getElementById("name");
+const nameform = document.getElementById("nameform");
+const name = document.getElementById("name");
 
-   let username='';
-   nameform.addEventListener('submit', function(event){
-       username = name.value;
-       event.preventDefault();
-       nameform.style.display ="none";
-       form.style.display ="block";
-   })
+let username='';
+nameform.addEventListener('submit', function(event){
+  username = name.value;
+  event.preventDefault();
+  nameform.style.display ="none";
+  form.style.display ="block";
+ })
 
-    form.addEventListener('submit', function(event){
-        const msg = JSON.stringify({msg: input.value, name: username})
-      socketio.emit('message', msg);
-      input.value='';
-      event.preventDefault();
-    })
+form.addEventListener('submit', function(event){
+  const msg = JSON.stringify({msg: input.value, name: username})
+  socketio.emit('message', msg);
+  input.value='';
+  event.preventDefault();
+ })
     socketio.on('message',function(msg){
         const obj = JSON.parse(msg);
 
@@ -27,6 +27,6 @@ const socketio = io();
       const dd = document.createElement("dd");
       dt.append(obj.name);
       chats.append(dt);
-      dd.append(obj.msg)
+      dd.append(obj.msg);
       chats.append(dd);
     });
