6845c52342929ad7f0f3a0b2f061b2ef23c01135
unknown <wakame@obirin.ac.jp>
2020-07-16 20:57:56 +0900

    からでは名前も文字も反映されない

--- public/main.js
+++ public/main.js
@@ -8,24 +8,32 @@ const name = document.getElementById("name");
 
 let username='';
 nameform.addEventListener('submit', function(event){
+ if(name.value!==''){
   username = name.value;
-  event.preventDefault();
   nameform.style.display ="none";
   form.style.display ="block";
 
   const msg = {msg: username + ' さんが参加しました。', name: 'システム'};
   socketio.emit('message', msg);
+ }
 
+ event.preventDefault();
 })
 
 form.addEventListener('submit', function(event){
-  const msg = {msg: input.value, name: username};
-  socketio.emit('message', msg);
-  input.value='';
+  if(input.value!==''){
+   const msg = {msg: input.value, name: username};
+   socketio.emit('message', msg);
+   input.value='';
+  }
   event.preventDefault();
  })
 
     socketio.on('message',function(msg){
+      if(username===''){
+        // まだ参加していなかったら、チャットを表示しない
+        return;
+      }
       const dt = document.createElement("dt");
       const dd = document.createElement("dd");
       dt.append(msg.name);
